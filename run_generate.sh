90775db110b094756b640d747d4f054318712ee9
DRXLSA <347632458@qq.com>
2023-03-30 22:09:57 +0800

    generate

--- run_generate.sh
+++ run_generate.sh
@@ -1,22 +1,39 @@
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3
 models="/data/lsj/nfs/moe/moe_model3"
-DATA="/data/lsj/nfs/moe/moe_data"
+spm=""
+input_path="/data/lsj/nfs/moe/moe_data"
 src="fi"
 tgt="en"
 max_tokens=4096
-resdir="/data/lsj/nfs/moe/moe_res"
+generate_dir="/data/lsj/nfs/moe/moe_res"
 lang_dict="en,fi" 
+split="test"
 
 python -m torch.distributed.launch --nproc_per_node=4 --master_addr="127.0.0.1" --master_port=12345 \
-generate.py $DATA \
+generate.py $input_path \
+--path "$models/checkpoint_last.pt"
 --task translation_multi_simple_epoch \
+--gen-subset  "$split" \
+--beam 3 \
 -s "$src" -t "$tgt" \
+--bpe  "sentencepiece"  --sentencepiece-model  "$spm" \
 --lang-pairs "$src-$tgt" --langs "$lang_dict" \
---is-moe --path "$models/checkpoint_last.pt" --fp16 \
---max-tokens "$max_tokens" --beam 3 --sacrebleu \
---remove-bpe --results-path $resdir \
---langtoks-specs "main" \
---langtoks "{\"main\":(\"src\", \"tgt\")}" \
---enable-lang-ids --ddp-backend "fully_sharded"
+--langtoks-specs "main" --langtoks "{\"main\":(\"src\", \"tgt\")}" \
+--max-tokens "$max_tokens" --scoring "sacrebleu" \
+--results-path $generate_dir \
+--is-moe  --fp16 --ddp-backend "fully_sharded" \
+--skip-invalid-size-inputs-valid-test \
+--enable-lang-ids 
 
+cat $generate_dir/generate-$split.txt | tail -1
+
+cat $generate_dir/generate-$split.txt | grep -P "^H" | sort -V | cut -f 3- > $generate_dir/decoding.txt
+
+spm_decode --model $spm \
+ --input $generate_dir/decoding.txt > $generate_dir/decoding.detok
+
+
+echo "====BLEU score is ...."
+raw_reference=$input_path/$split.$tgt
+cat $generate_dir/decoding.detok | sacrebleu -b $raw_reference
